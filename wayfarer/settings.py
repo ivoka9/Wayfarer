23aa3422c7466a20cd542e336eb521783832f358
ivoka <ivailo96g@gmail.com>
2020-07-03 21:03:24 -0400

    debug

--- wayfarer/settings.py
+++ wayfarer/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 't$g6ub6w8mm)!yv%mqn47)u9$n@*r2+1h$0(kxru&4svh^anpo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['wayfarerifi.herokuapp.com','127.0.0.1']
 
